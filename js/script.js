7990f06a8394370e5d72e565d88eade598405d55
Natasha1594 <kinuthianatasha@gmail.com>
2021-02-19 00:26:37 +0300

    add changes in js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,22 @@ const maleakannames = [
     "Kofi"
     "Kwame"
 ];
+
+
+//This fetchs data from all the inputs
+function fetchformdata() {
+let form = document.forms[0];
+let day, month, year, gender;
+
+gender = form.gender.value;
+
+day = parseInt(form.day.value);
+month = parseInt(form.month.value);
+year = parseInt(form.year.value);
+
+return [gender, day, month, year];
+
+}
+
+
+
