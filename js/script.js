b944aee218fe50c4a0d5def21a4c9a6be9455b4b
Natasha1594 <kinuthianatasha@gmail.com>
2021-02-19 00:09:56 +0300

    add js data

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//A list of the Akan female names from Sunday to Saturday
+const femaleakannames = [
+    "Akosua",
+    "Adwoa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+];
+
+//A list of the Akan male names from Sunday to Saturday
+const maleakannames = [
+    "Kwasi"
+    "Kwadwo"
+    "Kwabena"
+    "Kwaku"
+    "Yaw"
+    "Kofi"
+    "Kwame"
+];
